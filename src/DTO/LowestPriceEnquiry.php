21a9d1b0aa2da7439c3d30b8a6bd6cefd04f9e33
rescudero <rescudero@hiberus.com>
2023-09-21 08:56:40 +0200

    Create class LowestPriceEnquiry

--- src/DTO/LowestPriceEnquiry.php
+++ src/DTO/LowestPriceEnquiry.php
@@ -4,5 +4,167 @@ namespace App\DTO;
 
 class LowestPriceEnquiry implements PromotionEnquiryInterface
 {
+    private ?int $productId;
+
+    private ?int $quantity;
+
+    private ?string $requestLocation;
+
+    private ?string $voucherCode;
+
+    private ?string $requestDate;
+
+    private ?int $price;
+
+    private ?int $discountPrice;
+
+    private ?int $promotionId;
+
+    private ?string $promotion;
+
+    /**
+     * @return int|null
+     */
+    public function getProductId(): ?int
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param int|null $productId
+     */
+    public function setProductId(?int $productId): void
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int|null $quantity
+     */
+    public function setQuantity(?int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRequestLocation(): ?string
+    {
+        return $this->requestLocation;
+    }
+
+    /**
+     * @param string|null $requestLocation
+     */
+    public function setRequestLocation(?string $requestLocation): void
+    {
+        $this->requestLocation = $requestLocation;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getVoucherCode(): ?string
+    {
+        return $this->voucherCode;
+    }
+
+    /**
+     * @param string|null $voucherCode
+     */
+    public function setVoucherCode(?string $voucherCode): void
+    {
+        $this->voucherCode = $voucherCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRequestDate(): ?string
+    {
+        return $this->requestDate;
+    }
+
+    /**
+     * @param string|null $requestDate
+     */
+    public function setRequestDate(?string $requestDate): void
+    {
+        $this->requestDate = $requestDate;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int|null $price
+     */
+    public function setPrice(?int $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDiscountPrice(): ?int
+    {
+        return $this->discountPrice;
+    }
+
+    /**
+     * @param int|null $discountPrice
+     */
+    public function setDiscountPrice(?int $discountPrice): void
+    {
+        $this->discountPrice = $discountPrice;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPromotionId(): ?int
+    {
+        return $this->promotionId;
+    }
+
+    /**
+     * @param int|null $promotionId
+     */
+    public function setPromotionId(?int $promotionId): void
+    {
+        $this->promotionId = $promotionId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPromotion(): ?string
+    {
+        return $this->promotion;
+    }
+
+    /**
+     * @param string|null $promotion
+     */
+    public function setPromotion(?string $promotion): void
+    {
+        $this->promotion = $promotion;
+    }
+
 
 }
