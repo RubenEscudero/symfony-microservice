c5714001b4db702738c0bd1216e502c31f8b6b13
rescudero <rescudero@hiberus.com>
2023-10-03 13:36:37 +0200

    Fix tests

--- tests/unit/LowestPriceFilterTest.php
+++ tests/unit/LowestPriceFilterTest.php
@@ -9,6 +9,7 @@ use App\Tests\ServiceTestCase;
 
 class LowestPriceFilterTest extends ServiceTestCase
 {
+    /** @test */
     public function lowest_price_promotions_filtering_is_applied_correctly(): void
     {
         //Given
